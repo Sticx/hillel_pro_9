99c4a4a5f6c941a6b861dddfa27f8458ba111df5
Sticx <108312079+Sticx@users.noreply.github.com>
2022-10-03 00:22:26 +0300

    first

---
+++ main.js
@@ -0,0 +1,10 @@
+// const button = document.querySelector('button')
+//
+// button.addEventListener('click',function () {
+//     document.querySelector('p',function (){
+//         this.style.color
+//     })
+//
+// })
+
+const changeColor = e => e.style.color ? e.style.color = '' : e.style.color = 'orange';
